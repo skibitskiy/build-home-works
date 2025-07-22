a8c735588aa09e351c6e8cc35a87f4349e7ed685
moskibitskiy <maksimka719@gmail.com>
2025-07-22 20:29:53 +0500

    Написал бандлер

--- tasks/builder/bundle.js
+++ tasks/builder/bundle.js
@@ -1,6 +1,28 @@
 import fs from "node:fs";
 import path from "node:path";
 
+const template = (runtime, entryPath) => `
+const cache = {};
+const module = {};
+
+${runtime}
+
+const require = (path) => {
+  const resultFromCache = module[path] && module[path].exports
+  if (resultFromCache) {
+    return resultFromCache
+  }
+
+  module[path] = {}
+
+  cache[path](require, module[path]);
+
+  return module[path].exports
+}
+
+cache['${entryPath}'](require, module)
+`
+
 /**
  * Примерный алгоритм работы бандлера:
  * 1. Прочитать entry и собрать список всех вызовов require
@@ -16,15 +38,54 @@ import path from "node:path";
 /**
  * @param {string} entryPath - путь к entry бандлинга
  */
-export function bundle(entryPath) {}
+export function bundle(entryPath) {
+  const cache = new Map()
+
+  const modulePaths = [entryPath]
+
+  const createCacheSet = (modulePath, moduleCode) => {
+    return `cache['${modulePath}'] = (require, module) => {\n${moduleCode}\n}`
+  }
+
+  while (modulePaths.length > 0) {
+    const modulePath = modulePaths.pop()
+
+    if (cache.has(modulePath)) {
+      continue
+    }
+
+    const moduleCode = fs.readFileSync(modulePath, "utf-8");
+
+    const modulePathWithoutSrc = modulePath.replace(/(\.\/)?src\//gi, './')
+
+    cache.set(modulePathWithoutSrc, createCacheSet(modulePathWithoutSrc, moduleCode));
+
+    const dir = path.dirname(modulePath);
+
+    const requireCalls = searchRequireCalls(moduleCode, dir);
+
+    modulePaths.push(...requireCalls);
+  }
+
+  const cacheKeys = [...cache.keys()].reverse()
+
+  const code = cacheKeys.map((key) => {
+    return cache.get(key)
+  }).join('\n')
+
+  return template(code, cacheKeys.at(-1))
+}
 
 /**
  * Функция для поиска в файле вызовов require
  * Возвращает id модулей
  * @param {string} code 
  */
-function searchRequireCalls(code) {
-  return [...code.matchAll(/require\(('|")(.*)('|")\)/g)].map(
+function searchRequireCalls(code, dir) {
+  return [...code.matchAll(/require\(('|")(.*)('|")\)/g)]
+  .map(
     (item) => item[2]
-  );
+  ).map((name) => {
+    return path.join(dir, name)
+  });
 }
--- tasks/builder/index.js
+++ tasks/builder/index.js
@@ -1,8 +1,15 @@
 import fs from 'node:fs';
 
 import { bundle } from "./bundle.js";
+import path from 'node:path';
 
 const output = bundle(process.argv[2]);
 
-fs.mkdirSync('./dist');
-fs.writeFileSync(`./dist/main.js`, output);
+const dirname = import.meta.dirname
+
+try {
+    fs.rmSync(path.join(dirname, './dist'), { recursive: true  })
+} catch {}
+
+fs.mkdirSync(path.join(dirname, './dist'));
+fs.writeFileSync(path.join(dirname, './dist/main.js'), output);
