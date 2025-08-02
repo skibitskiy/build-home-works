44fb1c2898d5fdf38e5c0189ac6dde97438bdefa
kholstinin <kholstinin.da@gmail.com>
2025-08-02 22:43:08 +0500

    fix: project missed dep

--- tasks/project/rollup.config.js
+++ tasks/project/rollup.config.js
@@ -3,6 +3,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import html from "@rollup/plugin-html";
 import typescript from "@rollup/plugin-typescript";
+import replace from "@rollup/plugin-replace";
 
 export default {
   
